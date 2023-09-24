d079d7e7209e81545b9fd6fd7f1e9ae2f5ff43c8
hogsen <Ldzmmqm.163.com>
2023-09-24 20:41:33 +0800

    新增案列

--- 001-demo/index.md
+++ 001-demo/index.md
@@ -1,7 +1,24 @@
 # 思考
 
 1. 如何让图片自适应
+
+```css
+/* img的父容器要有确定的高宽 */
+img {
+	/* height是必须的(因为宽度会自适应),其它可选*/
+	height: 100%;
+	width: 100%;
+	object-fit: cover;
+	/* object-fit: contain; */
+}
+```
+
 2. 元素居中的方式
+
+- flex,grade 布局
+- 绝对定位+移动/margin
+- margin
+
 3. 如何给伪元素的 content 动态加内容
 
 ```
@@ -15,7 +32,8 @@ div::after{
 
 4. 如何创建元素
 5. 如何动态变更样式
-6. NodeList 与 HTMLCollection 的区别
+6. HTMLCollection 与 NodeList 的区别
 7. parentElement 与 parentNode 的区别
 8. 使用 fragment 优化代码
 9. 使用事件委托优化代码
+10. 当使用 typescript 时如何收敛类型范围
---
+++ 002-demo/script.js
@@ -0,0 +1,55 @@
+"use strict";
+var Progress_Steps;
+(function (Progress_Steps) {
+    const state = [{ id: 0, icon: '1' }, { id: 1, icon: '2' }, { id: 2, icon: '3' }, { id: 3, icon: '4' }, { id: 4, icon: '5' }];
+    const progress = document.querySelector('.progress-container');
+    const progressBar = document.querySelector('.bar');
+    const btnPrev = document.querySelector('#prev');
+    const btnNext = document.querySelector('#next');
+    function useCreateElement(targetElement, data) {
+        const fragme = document.createDocumentFragment();
+        data.forEach((item, index) => {
+            const element = document.createElement('div');
+            if (index === 0) {
+                element.classList.add('active');
+            }
+            element.classList.add('circle');
+            element.textContent = item.icon;
+            fragme.append(element);
+        });
+        targetElement.append(fragme);
+    }
+    useCreateElement(progress, state);
+    const circles = document.querySelectorAll('.circle');
+    let clickCount = 0, length = state.length - 1;
+    btnNext.addEventListener('click', () => {
+        clickCount++;
+        progressBar.style.width = 100 / length * clickCount + '%';
+        circles.item(clickCount).classList.add('active');
+        if (clickCount >= length) {
+            btnNext.disabled = true;
+        }
+        else if (clickCount <= 0) {
+            btnPrev.disabled = true;
+        }
+        else {
+            btnNext.disabled = false;
+            btnPrev.disabled = false;
+        }
+    });
+    btnPrev.addEventListener('click', () => {
+        circles.item(clickCount).classList.remove('active');
+        clickCount--;
+        progressBar.style.width = 100 / length * clickCount + '%';
+        if (clickCount >= length) {
+            btnNext.disabled = true;
+        }
+        else if (clickCount <= 0) {
+            btnPrev.disabled = true;
+        }
+        else {
+            btnNext.disabled = false;
+            btnPrev.disabled = false;
+        }
+    });
+})(Progress_Steps || (Progress_Steps = {}));
